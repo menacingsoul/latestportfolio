99d84d621f4d82fbba7e24bc98c04cc209ab3ac6
menacingsoul <adarsh14103@gmail.com>
2024-08-11 15:56:12 +0530

    removed ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,55 @@
+// app/page.tsx
+"use client";
 import Portfolio from "../components/Portfolio";
-import { prisma } from "../utils/db";
+import {
+  fetchAdarshDetails,
+  fetchPortfolioData,
+  fetchSkillData,
+} from "../utils/api";
+import { useEffect, useState } from "react";
+import { Loader2 } from "lucide-react";
 
-const MainPage = async () => {
-  const projects = await prisma.projects.findMany();
-  const skills = await prisma.skills.findMany();
-  const profile = await prisma.adarsh.findUnique({
-    where: {
-      id: "301feb70-7e9a-44c0-9a66-1b97b3caf0ee",
-    },
+const MainPage = () => {
+  const [projects, setProjects] = useState([]);
+  const [skills, setSkills] = useState([]);
+  const [profile, setAdarsh] = useState({
+    tagline: "",
+    bio: "",
+    email: "",
+    image: "",
+    resume: "",
+    github: "",
+    linkedin: "",
   });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const projectData = await fetchPortfolioData();
+      const skillData = await fetchSkillData();
+      const adarshData = await fetchAdarshDetails();
+      setProjects(projectData.projects);
+      setSkills(skillData.skills);
+      setAdarsh(adarshData);
+      setLoading(false);
+    };
+
+    loadData();
+  }, []);
 
   return (
     <>
-      <Portfolio projects={projects} skills={skills} adarsh={profile} />
+      {loading ? (
+        <div>
+          <div className="fixed inset-0 flex items-center justify-center z-50 bg-white bg-opacity-100">
+            <div className=" cursor-pointer overflow-hidden text-black p-8 ">
+              <Loader2 className="mr-2 h-16 w-16 animate-spin" />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Portfolio projects={projects} skills={skills} adarsh={profile} />
+      )}
     </>
   );
 };
